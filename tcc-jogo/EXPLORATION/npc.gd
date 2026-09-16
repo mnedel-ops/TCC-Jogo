3e797d546d2eb20ed8ddecc60eb3cae09f60ec18
mnedel-ops <m.nedel@ufn.edu.br>
2026-09-16 10:34:28 -0300

    Exploracao funcionando
    Movimentacao e trigger do combat. Ainda nao muda o scene

--- tcc-jogo/EXPLORATION/npc.gd
+++ tcc-jogo/EXPLORATION/npc.gd
@@ -5,17 +5,25 @@ class_name NPC
 @onready var is_zone : bool = false 
 @onready var dialogueBox : Label = $Root/Dialogo
 
+@export var figther : bool = false
+@onready var combat_collision: Area3D = $CombatCollision
+
+
 func _ready() -> void:
 	dialoguearea.area_entered.connect(_on_area_entered)
 	dialoguearea.area_exited.connect(_on_area_exited)
 	dialogueBox.visible = false
+	
+	if figther:
+		combat_collision.area_entered.connect(_on_combat_area_entered)
+		combat_collision.area_exited.connect(_on_combat_area_exited)
 
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_interact") and is_zone:
 		talk()
 
-
+#Func do dialogo
 func _on_area_entered(area: Area3D) -> void:
 	if area.is_in_group("player"):
 		is_zone = true
@@ -24,8 +32,16 @@ func _on_area_exited(area: Area3D) -> void:
 		is_zone = false
 		dialogueBox.visible =false
 
-
 func talk():
 	print("Estamos conversando...")
 	dialogueBox.visible = true
 	
+
+#Func do combate
+func _on_combat_area_entered(area: Area3D) -> void:
+	if area.is_in_group("player"):
+		print("Vou te quebrar na porrada")
+		get_tree().change_scene_to_file("res://COMBAT/combat_refactored.tscn")
+func _on_combat_area_exited(area: Area3D) -> void:
+	if area.is_in_group("player"):
+		print("Cansei de te espancar. Vaza")
