32612e952ea5bc1c186ad2ee28f14baf51085f7d
mnedel-ops <m.nedel@ufn.edu.br>
2026-09-21 09:22:03 -0300

    NPCs adicionados
    Agr os NPCs sao instanciados pelos markers

--- tcc-jogo/Level.gd
+++ tcc-jogo/Level.gd
@@ -3,6 +3,7 @@ class_name Level
 
 @export var npc_scene: PackedScene
 @onready var NPCHolder_guy: Node = $NPCHolder
+var Spawn_Array: Array[NPCSpawnPoint]
 
 signal existo
 
@@ -10,12 +11,20 @@ func _ready() -> void:
 	put_npcs()
 
 func put_npcs() -> void:
-	var placement = NPCHolder_guy.get_children()
+	for i in NPCHolder_guy.get_children():
+		if i is NPCSpawnPoint:
+			Spawn_Array.append(i)
+		
+	place_npcs()
 	
-	var npc_scene_guy := npc_scene.instantiate()
-	npc_scene_guy.position = placement[2].position as Vector3
-	npc_scene_guy.alchemon1 = placement[2].alchemons_ids[0]
-	npc_scene_guy.alchemon2 = placement[2].alchemons_ids[1]
-	npc_scene_guy.figther=true
-	NPCHolder_guy.add_child(npc_scene_guy)
-	existo.emit()
+func  place_npcs():
+	for marker:NPCSpawnPoint in Spawn_Array:
+		var npc_scene_guy := npc_scene.instantiate()
+		
+		npc_scene_guy.position = marker.position as Vector3
+		npc_scene_guy.alchemon1 = marker.alchemons_ids[0]
+		npc_scene_guy.alchemon2 = marker.alchemons_ids[1]
+		
+		npc_scene_guy.figther=true
+		NPCHolder_guy.add_child(npc_scene_guy)
+		existo.emit()
